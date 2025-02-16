bbdc86412ecaf1018d4bad201717fef94e053e45
hyjoong <maplesosser@naver.com>
2025-02-16 15:56:38 +0900

    feat: coral 색상코드 변경

--- lib/ui/colors.dart
+++ lib/ui/colors.dart
@@ -4,7 +4,7 @@ const mint = Color(0xFFB5EAD7);
 const peach = Color(0xFFFFB7B2);
 const lavender = Color(0xFFE2D1F9);
 const sage = Color(0xFFCBD18F);
-const coral = Color(0xFFFEC5BB);
+const coral = Color(0xFFFFF3B0);
 const powder = Color(0xFFBDE0FE);
 
 const darkGray = Color(0xFF202020);
